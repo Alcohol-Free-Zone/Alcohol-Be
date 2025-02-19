b452b60b65746834562474ba5906e56817b43a81
dhrudcks01 <dhrudcks01@naver.com>
2025-02-19 23:55:22 +0900

    기본 Spring-boot 구성

---
+++ src/main/java/com/example/alcoholbe/AlcoholBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.alcoholbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlcoholBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlcoholBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/alcoholbe/AlcoholBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alcoholbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlcoholBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
