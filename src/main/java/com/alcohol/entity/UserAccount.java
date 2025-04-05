2833257a546ee7b5e404cbb5078c01caf0fc007f
Kimjunseon <junseon0722@gmail.com>
2025-04-05 22:51:59 +0900

    🗃 fix: UserAccount 테이블 패스워드 길이 수정

--- src/main/java/com/alcohol/entity/UserAccount.java
+++ src/main/java/com/alcohol/entity/UserAccount.java
@@ -18,7 +18,7 @@ public class UserAccount {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 20)
+    @Column(nullable = false, length = 200)
 	private String password;
 
     
