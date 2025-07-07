a3b2805f46c798ff1613239d59b24abb62dc9f29
kimjunseon-sainti <junseon0722@sainti.co.kr>
2025-07-07 22:26:18 +0900

    :fire: 엔터티 수정 및 이름 변경

--- src/main/java/com/alcohol/application/pet/dto/PetResponseDto.java
+++ src/main/java/com/alcohol/application/pet/dto/PetResponseDto.java
@@ -1,5 +1,7 @@
 package com.alcohol.application.pet.dto;
 
+import java.util.Date;
+
 import com.alcohol.application.pet.entity.Pet;
 
 import lombok.Builder;
@@ -8,13 +10,23 @@ import lombok.Getter;
 @Getter
 @Builder
 public class PetResponseDto {
-    private Long id;
+    private Long petId;
     private String imgUrl;
+    private Date birth;
+    private String memo;
+    private String petName;
+    private String breed;
+    private int petAge;
 
     public static PetResponseDto from(Pet petList) {
         return PetResponseDto.builder()
-                .id(petList.getPetId())
+                .petId(petList.getPetId())
                 .imgUrl(petList.getImgUrl())
+                .birth(petList.getBirth())
+                .memo(petList.getMemo())
+                .petName(petList.getPetName())
+                .breed(petList.getBreed())
+                .petAge(petList.getPetAge())
                 .build();
     }
 }
--- src/main/java/com/alcohol/application/pet/entity/Pet.java
+++ src/main/java/com/alcohol/application/pet/entity/Pet.java
@@ -1,12 +1,20 @@
 package com.alcohol.application.pet.entity;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
+import com.alcohol.application.userAccount.entity.UserAccount;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -45,5 +53,15 @@ public class Pet {
     // 반려동물 나이 
     @Column(nullable = false)
     private int petAge;
+
+    @OneToMany(mappedBy = "pet")
+    private List<PetPersonalityTag> personalityTags = new ArrayList<>();
+
+    @OneToMany(mappedBy = "pet")
+    private List<PetAnniversary> petAnniversaries = new ArrayList<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserAccount userAccount;
 
 }
