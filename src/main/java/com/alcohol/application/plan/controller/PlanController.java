e47da5c18610dafd70545a79749485e29608194a
Kimjunseon <junseon0722@gmail.com>
2025-07-23 01:33:55 +0900

    :sparkles: feat: 일정 controller 추가

---
+++ src/main/java/com/alcohol/application/plan/controller/PlanController.java
@@ -0,0 +1,41 @@
+package com.alcohol.application.plan.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/plan")
+public class PlanController {
+
+    // 유저 일정 리스트 조회
+    @GetMapping
+    public ResponseEntity<?> getPlanListByUser() {
+        return ResponseEntity.ok(null);
+    }
+
+    // 일정 상세조회
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getPlanById(@PathVariable Long id) {
+        return ResponseEntity.ok(null);
+    }
+
+    // 일정 추가, 수정하기
+    @PostMapping
+    public ResponseEntity<?> createOrUpdatePlan() {
+        return ResponseEntity.ok(null);
+    }
+
+    // 일정 삭제하기
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletePlan(@PathVariable Long id) {
+        return ResponseEntity.ok(null);
+    }
+}
