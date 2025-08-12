3944faa4ea4bca4a169ddbd81c5c73a58bd6f917
kimjunseon-sainti <junseon0722@sainti.co.kr>
2025-08-12 15:49:04 +0900

    ✨ feat: 성격 태그 종류 추가

--- src/main/java/com/alcohol/application/Enum/PersonalityTagType.java
+++ src/main/java/com/alcohol/application/Enum/PersonalityTagType.java
@@ -1,13 +1,23 @@
 package com.alcohol.application.Enum;
 
 public enum PersonalityTagType {
-    활동("#활동"),
     온순("#온순"),
-    호기심("#호기심"),
-    충성("#충성"),
-    장난꾸러기("#장난꾸러기");
+    활발("#활발"),
+    장난기많음("#장난기많음"),
+    애교많음("#애교많음"),
+    독립적임("#독립적임"),
+    호기심많음("#호기심많음"),
+    겁많음("#겁많음"),
+    충성심강함("#충성심강함"),
+    사교적임("#사교적임"),
+    느긋함("#느긋함"),
+    영리함("#영리함"),
+    조심성많음("#조심성많음"),
+    집요함("#집요함"),
+    민첩함("#민첩함"),
+    탐험심강함("#탐험심강함");
 
-    private String tag;
+    private final String tag;
 
     PersonalityTagType(String tag) {
         this.tag = tag;
